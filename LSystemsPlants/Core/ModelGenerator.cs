1aed6b5fb4db02718984f2032a525efdf52f8eb2
YurasovDV <jurasovdvx1@rambler.ru>
2017-02-05 19:08:38 +0400

    added stub for L-systems project

---
+++ LSystemsPlants/Core/ModelGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace LSystemsPlants.Core
+{
+    class ModelGenerator
+    {
+        public SimpleModel Generate()
+        {
+            return new SimpleModel();
+        }
+    }
+}
---
+++ LSystemsPlants/View/MainForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LSystemsPlants
+{
+    public partial class MainForm : Form
+    {
+        private Engine _engine;
+        private Timer _timer;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _engine = new Engine();
+
+            _timer = new Timer()
+            {
+                Interval = 16
+            };
+            _timer.Tick += _timer_Tick;
+            _timer.Start();
+        }
+
+        private void _timer_Tick(object sender, EventArgs e)
+        {
+            _engine.Tick(_timer.Interval);
+        }
+
+        private void RenderFrame()
+        {
+            portraitControl.SwapBuffers();
+            portraitControl.Refresh();
+        }
+    }
+}
