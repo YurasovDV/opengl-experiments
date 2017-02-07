7b9aaaa4bc6860d89710918095fd362c599a1ee7
YurasovDV <jurasovdvx1@rambler.ru>
2017-02-07 23:13:55 +0400

    added missed files

---
+++ LSystemsPlants/Core/Graphics/RenderEngine.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics.OpenGL4;
+
+namespace LSystemsPlants.Core.Graphics
+{
+    class RenderEngine
+    {
+
+        private ShaderManager _shaders;
+
+        public Matrix4 ModelView = Matrix4.Identity;
+
+        public Matrix4 Projection = Matrix4.Identity;
+
+        public Matrix4 ModelViewProjection = Matrix4.Identity;
+
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public RenderEngine(int width, int height, float zFar = 200)
+        {
+            Width = width;
+            Height = height;
+
+            GL.Viewport(0, 0, (int)width, (int)height);
+            float aspect = width / height;
+
+            Projection = Matrix4.CreatePerspectiveFieldOfView(0.5f, aspect, 0.1f, zFar);
+
+            _shaders = new ShaderManager();
+
+        }
+
+        public void Render(double delta)
+        {
+
+
+            GL.Flush();
+        }
+    }
+}
---
+++ LSystemsPlants/Core/L_Systems/Symbol.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSystemsPlants.Core.L_Systems
+{
+    enum Symbol
+    {
+        /// <summary>
+        /// F
+        /// </summary>
+        FORWARD_DRAW = 1,
+
+        /// <summary>
+        /// +
+        /// </summary>
+        TURN_LEFT = 2,
+
+        /// <summary>
+        /// -
+        /// </summary>
+        TURN_RIGHT = 3,
+
+        /// <summary>
+        /// f
+        /// </summary>
+        FORWARD_NO_DRAW = 4
+    }
+}
