dedf9b94f80dd502e10c24ce122c3ff3ad6becf7
YurasovDV <jurasovdvx1@rambler.ru>
2017-02-24 02:09:16 +0400

    added settings

--- LSystemsPlants/Core/L_Systems/GeneratorSettings.cs
+++ LSystemsPlants/Core/L_Systems/GeneratorSettings.cs
@@ -2,6 +2,24 @@
 {
    public class GeneratorSettings
     {
+        public static GeneratorSettings Default = new GeneratorSettings()
+        {
+            MaxIteration = 5,
+            InitialDelta = 0.5f,
+            InitialStep = 30,
+            DeltaChangeAtEveryLevel = 0.75f,
+            StepChangeAtEveryLevel = 0.75f
+        };
+
         public int MaxIteration { get; set; }
+
+        public float InitialDelta { get; set; }
+
+        public float InitialStep { get; set; }
+
+        public float DeltaChangeAtEveryLevel { get; set; }
+
+        public float StepChangeAtEveryLevel { get; set; }
+
     }
 }
