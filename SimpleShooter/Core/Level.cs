dc6ff4db808be76413277d5859c3cbf47b8356b7
YurasovDV <jurasovdvx1@rambler.ru>
2017-04-29 02:01:34 +0400

    fixed build: level.cs

---
+++ SimpleShooter/Core/Level.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using OpenTK;
+using SimpleShooter.Player;
+
+namespace SimpleShooter.Core
+{
+    public class Level
+    {
+        public Vector3 LightPosition { get; internal set; }
+        public IEnumerable<GameObject> Objects { get; set; }
+        public PlayerModel Player { get; set; }
+    }
+}
